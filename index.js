f156170dcffcecc7c93f490b85f0b4552553d6cf
Agustin Paul <a.agustinpaul@gmail.com>
2022-11-08 15:33:42 -0300

    Clase01

---
+++ index.js
@@ -0,0 +1,16 @@
+/*{
+    nombreCliente,
+    dniCliente;
+    numeroCuenta;
+    saldoCuenta;
+}*/
+
+const nombreCliente = "Leonardo";
+const dniCliente = "1625354633";
+const numeroCuenta = "12344654674";
+const saldoCuenta = 1000;
+
+const nombreCliente2 = "Leonardo";
+const dniCliente2 = "1625354633";
+const numeroCuenta2 = "12344654674";
+const saldoCuenta2 = 1000;
