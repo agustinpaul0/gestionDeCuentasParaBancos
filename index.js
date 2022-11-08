1dfd48677bab5daffd166b05df4c3f8d30e92e65
Agustin Paul <a.agustinpaul@gmail.com>
2022-11-08 15:49:33 -0300

    Clase02

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-/*{
-    nombreCliente,
+class Cliente
+{
+    nombreCliente;
     dniCliente;
     numeroCuenta;
     saldoCuenta;
-}*/
+    rutCliente;
+}
 
-const nombreCliente = "Leonardo";
+const cliente1 = new Cliente();
+
+cliente1.nombreCliente = "José";
+cliente1.dniCliente = "126446373";
+cliente1.numeroCuenta = "3535463";
+cliente1.saldoCuenta = 2000;
+
+console.log(cliente1);
+
+const cliente2 = new Cliente();
+
+cliente2.nombreCliente = "Leonardo";
+cliente2.dniCliente = "1625354633";
+cliente2.numeroCuenta = "12344654674";
+cliente2.saldoCuenta = 1000;
+
+console.log(cliente2);
+
+const cliente3 = new Cliente();
+
+cliente3.nombreCliente = "Juan";
+cliente3.dniCliente = "000912029";
+cliente3.numeroCuenta = "55464647";
+cliente3.saldoCuenta = 500;
+
+console.log(cliente3);
+
+/*const nombreCliente = "Leonardo";
 const dniCliente = "1625354633";
 const numeroCuenta = "12344654674";
 const saldoCuenta = 1000;
@@ -14,3 +43,6 @@ const nombreCliente2 = "Leonardo";
 const dniCliente2 = "1625354633";
 const numeroCuenta2 = "12344654674";
 const saldoCuenta2 = 1000;
+
+console.log(nombreCliente);
+console.log(dniCliente);*/
